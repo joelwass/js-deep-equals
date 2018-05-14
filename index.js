74d7e95679c1457c85252876dade9d51919b3fbe
joelwass <joel.wasserman8@gmail.com>
2018-05-14 17:24:20 -0400

    added non recursive function

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const mapify = (arr1, retObject = {}, currKey = '') => {
+const mapifyRecursive = (arr1, retObject = {}, currKey = '') => {
   const isObject = typeof arr1 === 'object'
   const isArray = Array.isArray(arr1)
   const keys = Object.keys(arr1)
@@ -18,6 +18,38 @@ const mapify = (arr1, retObject = {}, currKey = '') => {
       }
     }
   })
+  return retObject
+}
+
+const mapify = (arr1) => {
+  const stillGoing = true
+  const retObject = {}
+  let currObject = arr1
+  let currKey = ''
+  
+  while (stillGoing) {
+    const isObject = typeof currObject === 'object'
+    const isArray = Array.isArray(currObject)
+    const keys = Object.keys(currObject)
+    keys.forEach(key => {
+      if (Array.isArray(currObject[key]) || typeof currObject[key] === 'object') {
+        if (isArray) {
+          currObject = currObject[key]
+          currKey = currKey + '[' + key
+        } else if (isObject) {
+          currObject = currObject[key]
+          currKey = currKey + '.' + key
+        }
+      } else {
+        if (isArray) {
+          retObject[currKey + '[' + key] = currObject[key]
+        } else if (isObject) {
+          retObject[currKey + '.' + key] = currObject[key]
+        }
+      }
+    })
+  }
+
   return retObject
 }
 
