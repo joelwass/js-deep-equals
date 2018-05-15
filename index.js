adaa51036f677c0221ce33ecf143c8b2f6bd500e
joelwass <joel.wasserman8@gmail.com>
2018-05-15 10:14:50 -0400

    consolidate

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const compare = (arr1, arr2) => {
   if (Array.isArray(arr1) && Array.isArray(arr2)) {
-    if (arr1.length !== arr2.length) {
-      // a man's array does not look like a girl's array
-      return false
-    }
+    // a man's array does not look like a girl's array
+    if (arr1.length !== arr2.length) return false
   }
 
   const keys = Object.keys(arr1)
@@ -11,10 +9,8 @@ const compare = (arr1, arr2) => {
     const key = keys[i]
     if (Array.isArray(arr1[key])) {
       if (Array.isArray(arr2[key])) {
-        if (!compare(arr1[key], arr2[key])) {
-          // a man has an array, a girl has a different array
-          return false
-        }
+        // a man has an array, a girl has a different array
+        if (!compare(arr1[key], arr2[key])) return false
         // a man may share an array with a girl
         continue
       }
@@ -24,19 +20,15 @@ const compare = (arr1, arr2) => {
 
     if (typeof arr1[key] === 'object') {
       if (typeof arr2[key] === 'object') {
-        if (!compare(arr1[key], arr2[key])) {
-          // a man has an object, a girl has a different object
-          return false
-        }
+        // a man has an object, a girl has a different object
+        if (!compare(arr1[key], arr2[key])) return false
         continue
       }
       // a man has an object, a girl does not
       return false
     }
-    if (arr1[key] !== arr2[key]) {
-      // a man has values that a girl does not share
-      return false
-    }
+    // a man has values that a girl does not share
+    if (arr1[key] !== arr2[key]) return false
   }
 
   return true
